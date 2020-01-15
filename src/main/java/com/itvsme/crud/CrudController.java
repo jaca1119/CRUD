d3350f961a8bc663e4bf0165a30adcaa15d93c1f
jaca1119 <laki1663@gmail.com>
2020-01-15 20:36:28 +0100

    Refactoring unused templates and mapping

--- src/main/java/com/itvsme/crud/CrudController.java
+++ src/main/java/com/itvsme/crud/CrudController.java
@@ -21,51 +21,9 @@ public class CrudController
         repository.save(new SimpleText("The Dust"));
     }
 
-
-//    @PostMapping("/add")
-//    public SimpleText newText(@RequestBody SimpleText text)
-//    {
-//        return repository.save(text);
-//    }
-
     @GetMapping("/allObjects")
     public String allObjects()
     {
         return "allObjects";
     }
-
-    @GetMapping("/add")
-    public String greetingForm(Model model)
-    {
-        model.addAttribute("simpleText", new SimpleText());
-        return "add";
-    }
-
-    @PostMapping("/add")
-    public String greetingSubmit(@ModelAttribute SimpleText greeting)
-    {
-        repository.save(greeting);
-        return "texts";
-    }
-
-    @GetMapping("/all")
-    public String showAll(Model model) {
-        model.addAttribute("simpleText", repository.findAll());
-        model.addAttribute("simpleTextUpdate", new SimpleText());
-        return "/all";
-    }
-
-
-
-    @PostMapping("/all")
-    public String update(@ModelAttribute SimpleText update) {
-        System.out.println(repository.findByID(update.getID()));
-        System.out.println(update.getID());
-        System.out.println(update.getText());
-//        repository.findByID(update.getID()).setText(update.getText());
-
-        return "/all";
-    }
-
-
 }
